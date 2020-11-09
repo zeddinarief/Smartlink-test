6f11cfbd6e4a4d3d5272e08b7d2a2d57a4c9fbd2
zeddin arief <zeddinarief@gmail.com>
2020-11-09 19:42:02 +0700

    Update settings.py

--- smartlaundryapi/smartlaundryapi/settings.py
+++ smartlaundryapi/smartlaundryapi/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
+
+Code by Zeddin Arief https://github.com/zeddinarief/Smartlink-test/
 """
 
 from pathlib import Path
